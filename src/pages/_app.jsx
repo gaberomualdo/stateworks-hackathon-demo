88f5c1173372f7b9e64554ff1b218b4d96ad6431
xtrp <36894883+xtrp@users.noreply.github.com>
2022-10-27 13:39:04 -0400

    feat: fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,23 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
-import { WagmiConfig, createClient } from 'wagmi'
-import { getDefaultProvider } from 'ethers'
+import {
+  WagmiConfig,
+  createClient,
+  configureChains,
+  chain,
+  defaultChains,
+} from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet, chain.polygon],
+  [publicProvider()]
+)
 
 const client = createClient({
   autoConnect: true,
-  provider: getDefaultProvider(),
+  provider,
+  webSocketProvider,
 })
 
 export default function App({ Component, pageProps }) {
